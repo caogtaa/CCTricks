21e95cd505231cdc74731746aa734f12a2d14ab7
Cao Gaoting <caogtaa@gmail.com>
2020-07-06 01:58:08 +0800

    add welcome

---
+++ assets/SceneWelcome.ts
@@ -0,0 +1,12 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class SceneTest extends cc.Component {
+    onLoad() {
+    }
+
+    onBtnClick(e) {
+        let name = e.currentTarget.name;
+        cc.director.loadScene(name);
+    }
+}
