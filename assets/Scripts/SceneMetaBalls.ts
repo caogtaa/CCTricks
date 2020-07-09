8b9107fb4fc778633d520e7de9b8c0b1a4a514b8
Cao Gaoting <caogtaa@gmail.com>
2020-07-09 16:23:41 +0800

    partially to ts

---
+++ assets/Scripts/SceneMetaBalls.ts
@@ -0,0 +1,128 @@
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class SceneMetaBalls extends cc.Component {
+	@property(cc.Camera)
+	camera_water: cc.Camera = null;
+
+	@property(cc.Sprite)
+	sp_water_show: cc.Sprite = null;
+
+	@property(cc.Node)
+	shuiLongTou: cc.Node = null;
+
+	protected pymanager: cc.PhysicsManager = null;
+	protected particleSystem: cc.ParticleSystem = null;
+
+	onLoad() {
+		this.pymanager = cc.director.getPhysicsManager();
+		this.pymanager.start();
+
+		const texture = new cc.RenderTexture();
+		let size = this.sp_water_show.node.getContentSize();
+        texture.initWithSize(size.width, size.height);
+		
+        const spriteFrame = new cc.SpriteFrame();
+        spriteFrame.setTexture(texture);
+        this.camera_water.targetTexture = texture;
+        this.sp_water_show.spriteFrame = spriteFrame;
+	}
+
+	initParticle(){
+		let PTM_RATIO = cc.PhysicsManager.PTM_RATIO;
+		var shuiLongTouSize = this.shuiLongTou.getContentSize();
+		var shuiLongTouPos = this.shuiLongTou.getPosition();
+		var size = cc.winSize;
+		this.particleSystem = this.pymanager._particles;
+		var box = new b2.PolygonShape();
+		box.SetAsBox(shuiLongTouSize.width/2/PTM_RATIO, (shuiLongTouSize.height * 1.8)/PTM_RATIO, new b2.Vec2(0, 0), 0);
+
+		var particleGroupDef = new b2.ParticleGroupDef();
+		particleGroupDef.shape = box;
+		particleGroupDef.flags = b2.waterParticle;
+		particleGroupDef.position.Set((shuiLongTouPos.x + size.width/2)/PTM_RATIO,(shuiLongTouPos.y + size.height/2 + shuiLongTouSize.height* 1.2)/PTM_RATIO);
+		this.particleGroup = this.particleSystem.CreateParticleGroup(particleGroupDef);
+		let vertsCount = this.particleSystem.GetParticleCount();
+		this.totalCount = vertsCount;
+		this._mat = this.sp_water_show.getMaterial(0);
+		this._mat.setProperty('resolution',new Float32Array(4));
+		this._mat.setProperty('metaballs',new Float32Array(vertsCount * 4));
+		console.log(vertsCount);
+	}
+
+    generateWater() {
+        this.resetWater();
+		setTimeout(()=>{
+			this.initParticle();
+			this.schedule(this.scheduleWater, 0.01);
+		},500);
+	}
+	
+    scheduleWater() {
+		let PTM_RATIO = cc.PhysicsManager.PTM_RATIO;
+		if(this.particleSystem != null){
+			let tmp = [];
+			let vertsCount = this.particleSystem.GetParticleCount();
+			let posVerts = this.particleSystem.GetPositionBuffer();
+			for (let i = 0; i < vertsCount; i++) {
+				tmp.push(posVerts[i].x * PTM_RATIO);
+				tmp.push(posVerts[i].y * PTM_RATIO);
+				tmp.push(0.35 * PTM_RATIO);
+				tmp.push(0.0);
+			}
+			let size = this.shuiLongTou.getContentSize();
+			if(this._mat){
+				this._mat.setProperty('resolution',[640.0,1136.0,vertsCount,this.shuiLongTou.y - size.height/2 + 568]);
+				this._mat.setProperty('metaballs',tmp);
+			}
+		}
+    }
+
+    resetWater() {
+        this.unschedule(this.scheduleWater);
+		/*
+		if(this._mat){
+			this._mat.setProperty('metaballs',new Float32Array(this.totalCount * 4));
+		}
+		*/
+		if(this.particleSystem != null){
+			this.particleGroup.DestroyParticles(null);
+			this.particleSystem.DestroyParticleGroup(this.particleGroup);
+			this.particleSystem = null;
+		}
+    }
+}
+
+
+// cc.Class({
+//     extends: cc.Component,
+
+//     properties: {
+// 		camera_water: cc.Camera,
+// 		sp_water_show: cc.Sprite,
+// 		shuiLongTou:cc.Node,
+//     },
+//     onLoad() {
+// 		this.pymanager = cc.director.getPhysicsManager();
+// 		this.pymanager.start()
+// 		/*
+// 		this.pymanager.debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |
+// 			cc.PhysicsManager.DrawBits.e_pairBit |
+// 			cc.PhysicsManager.DrawBits.e_centerOfMassBit |
+// 			cc.PhysicsManager.DrawBits.e_jointBit |
+// 			cc.PhysicsManager.DrawBits.e_shapeBit //|
+// 			//cc.PhysicsManager.DrawBits.e_particleBit
+// 			;
+// 			*/
+// 		const texture = new cc.RenderTexture();
+// 		let size = this.sp_water_show.node.getContentSize();
+//         texture.initWithSize(size.width, size.height);
+		
+//         const spriteFrame = new cc.SpriteFrame();
+//         spriteFrame.setTexture(texture);
+//         this.camera_water.targetTexture = texture;
+//         this.sp_water_show.spriteFrame = spriteFrame;
+//     },
+
+// });
