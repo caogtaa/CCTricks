ecd9f8b2acd85d29cf9c6c1c915a3e5f2ec99a3d
Cao Gaoting <caogtaa@gmail.com>
2020-08-19 22:48:29 +0800

    replace hot code and do low level optimize

--- assets/Scripts/Scene/SceneMetaBalls.ts
+++ assets/Scripts/Scene/SceneMetaBalls.ts
@@ -113,3 +113,94 @@ export default class SceneMetaBalls extends cc.Component {
 		}
     }
 }
+
+
+cc.game.once(cc.game.EVENT_ENGINE_INITED, () => {
+	// b2ParticleSystem.prototype.FindContacts_Reference = function (contacts) {
+	b2.ParticleSystem.prototype.FindContacts_Reference = function (contacts) {
+		if (!this.m_flagsBuffer.data) {
+			throw new Error();
+		}
+		if (!this.m_positionBuffer.data) {
+			throw new Error();
+		}
+
+		// DEBUG: b2Assert(contacts === this.m_contactBuffer);
+		var beginProxy = 0;
+		var endProxy = this.m_proxyBuffer.count;
+		this.m_contactBuffer.count = 0;
+		let contactBuffer = this.m_contactBuffer;
+		var proxyData = this.m_proxyBuffer.data;
+		var count = 0;
+		var computeRelativeTag = b2.ParticleSystem.computeRelativeTag;
+		var AddContact = this.AddContact2.bind(this);
+		for (var a = beginProxy, c = beginProxy; a < endProxy; a++) {
+			var dataA = proxyData[a];
+			var tagA = dataA.tag;
+			var indexA = dataA.index;
+			var rightTag = computeRelativeTag(tagA, 1, 0);
+			for (var b = a + 1; b < endProxy; b++) {
+				++ count;
+				if (rightTag < proxyData[b].tag) {
+					break;
+				}
+				AddContact(indexA, proxyData[b].index, contactBuffer);
+			}
+			var bottomLeftTag = computeRelativeTag(tagA, -1, 1);
+			for (; c < endProxy; c++) {
+				++ count;
+				if (bottomLeftTag <= proxyData[c].tag) {
+					break;
+				}
+			}
+			var bottomRightTag = computeRelativeTag(tagA, 1, 1);
+			for (var b = c; b < endProxy; b++) {
+				++ count;
+				if (bottomRightTag < proxyData[b].tag) {
+					break;
+				}
+				AddContact(indexA, proxyData[b].index, contactBuffer);
+			}
+		}
+	};
+
+	//@ts-ignore
+	var AddContact_s_d = new b2.Vec2();
+	//@ts-ignore
+	b2.ParticleSystem.prototype.AddContact2 = function (a, b, contacts) {
+		var s_d = AddContact_s_d;
+		var pos_data = this.m_positionBuffer.data;
+		// DEBUG: b2Assert(contacts === this.m_contactBuffer);
+		///b2Vec2 d = m_positionBuffer.data[b] - m_positionBuffer.data[a];
+
+		var ax = pos_data[a].x;
+		var ay = pos_data[a].y;
+		var bx = pos_data[b].x;
+		var by = pos_data[b].y;
+
+		var dx = bx - ax;
+		var dy = by - ay;
+		// var d = b2.Vec2.SubVV(pos_data[b], pos_data[a], s_d);
+
+		var distBtParticlesSq = dx * dx + dy * dy;
+		// var distBtParticlesSq = b2.Vec2.DotVV(d, d);
+		if (distBtParticlesSq < this.m_squaredDiameter) {
+			var invD = 1 / Math.sqrt(distBtParticlesSq);
+			// var invD = b2.InvSqrt(distBtParticlesSq);
+			if (!isFinite(invD)) {
+				invD = 1.98177537e+019;
+			}
+			///b2ParticleContact& contact = contacts.Append();
+			var contact = this.m_contactBuffer.data[this.m_contactBuffer.Append()];
+			contact.indexA = a;
+			contact.indexB = b;
+			contact.flags = this.m_flagsBuffer.data[a] | this.m_flagsBuffer.data[b];
+			contact.weight = 1 - distBtParticlesSq * invD * this.m_inverseDiameter;
+			///contact.SetNormal(invD * d);
+
+			contact.normal.x = invD * dx;
+			contact.normal.y = invD * dy;
+			// b2.Vec2.MulSV(invD, d, contact.normal);
+		}
+	};
+});
