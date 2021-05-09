28951106831542cf532118d2d72db547b654e8e4
Cao Gaoting <caogtaa@gmail.com>
2021-05-09 20:53:19 +0800

    make it works

--- assets/Demo/VisualizeMusic/SceneVisualizeMusic.ts
+++ assets/Demo/VisualizeMusic/SceneVisualizeMusic.ts
@@ -16,12 +16,17 @@ export default class SceneVisualizeMusic extends cc.Component {
     @property(cc.AudioClip)
     clip: cc.AudioClip = null;
 
+    @property(cc.Sprite)
+    sprite: cc.Sprite = null;
+
     protected _analyser: any = null;
-    protected _freqSize: number = 128;
+    protected _freqSize: number = 1024;
     protected _gainNode: any = null;
     protected _ac: any = null;
     protected _audioId: number = -1;
-    protected _freqBuff: Uint8Array;
+    protected _freqBuff: Uint8Array = null;
+
+    protected _texture: cc.RenderTexture = null;
 
 
     onLoad() {
@@ -32,15 +37,19 @@ export default class SceneVisualizeMusic extends cc.Component {
         if (!this.clip)
             return;
 
+        // 参考引擎源码
+        // cocos2d\audio\CCAudio.js
         let audioId = this._audioId = cc.audioEngine.playMusic(this.clip, true);
+        //@ts-ignore
         let audio = cc.audioEngine._id2audio[audioId];
         let element = audio?._element;
         let buffer = element?._buffer;
-        let ac = element?._context;
-        if (!buffer || !ac)
+        let audioContext = element?._context;
+        if (!buffer || !audioContext)
             return;
 
-        let analyser = this._analyser = ac?.createAnalyser();
+        // https://developer.mozilla.org/zh-CN/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API
+        let analyser = this._analyser = audioContext?.createAnalyser();
         if (!analyser) {
             console.warn("Platform not support audio analyse");
             return;
@@ -48,13 +57,26 @@ export default class SceneVisualizeMusic extends cc.Component {
         
         analyser.fftSize = this._freqSize * 2;
 
-        //this._gainNode = ac[ac.createGain ? "createGain":"createGainNode"]();
-        //this._gainNode.connect(ac.destination);
         // analyser.connect(element._gainObj);
         // analyser.connect(element._currentSource);
-        element._currentSource?.connect(analyser);
+        // element._currentSource?.connect(analyser);
+        element._gainObj.connect(analyser);
 
         this._freqBuff = new Uint8Array(analyser.frequencyBinCount);
+
+        let gl = cc.game._renderContext;
+        let texture = this._texture = new cc.RenderTexture;
+        let width = analyser.frequencyBinCount;
+        texture.initWithSize(width, 1, gl.STENCIL_INDEX8);
+        texture.packable = false;
+
+        // 像素化
+        // texture.setFilters(cc.Texture2D.Filter.NEAREST, cc.Texture2D.Filter.NEAREST);
+
+        // 每个像素只需要1 byte表示，用I8格式
+        texture.initWithData(this._freqBuff, cc.Texture2D.PixelFormat.I8, width, 1);
+
+        this.sprite.spriteFrame = new cc.SpriteFrame(texture);
     }
 
     onDestroy() {
@@ -66,10 +88,18 @@ export default class SceneVisualizeMusic extends cc.Component {
         if (!analyser || !this._freqBuff)
             return;
 
+        // 获取频域数据
+        analyser.getByteFrequencyData(this._freqBuff);
 
-        // analyser.getByteFrequencyData(this._freqBuff);
-        analyser.getByteTimeDomainData(this._freqBuff);
-        // self.draw(arr);
-        let k = 0;
+        // 获取时域数据
+        //analyser.getByteTimeDomainData(this._freqBuff);
+
+        // 音频数据更新到纹理
+        let texture = this._texture;
+        //@ts-ignore
+        let opts = texture._getOpts();
+        opts.image = this._freqBuff;
+        opts.format = cc.Texture2D.PixelFormat.I8;
+        this._texture.update(opts);
     }
 }
