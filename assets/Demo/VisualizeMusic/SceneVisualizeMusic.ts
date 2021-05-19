7d8f9c0bc01b2dac027e42166b1bf37009c31e21
Cao Gaoting <caogtaa@gmail.com>
2021-05-19 20:04:24 +0800

    calculate fft data offline

--- assets/Demo/VisualizeMusic/SceneVisualizeMusic.ts
+++ assets/Demo/VisualizeMusic/SceneVisualizeMusic.ts
@@ -20,18 +20,109 @@ export default class SceneVisualizeMusic extends cc.Component {
     sprite: cc.Sprite = null;
 
     protected _analyser: any = null;
-    protected _freqSize: number = 256;   // 1024, be pow of 2
+    protected _freqSize: number = 32;   // 1024, be pow of 2
     protected _gainNode: any = null;
     protected _ac: any = null;
     protected _audioId: number = -1;
     protected _freqBuff: Uint8Array = null;
+    protected _sourceNode: AudioBufferSourceNode = null;
 
     protected _texture: cc.RenderTexture = null;
-
+    protected _fftTexture: Uint8Array = null;
 
     onLoad() {
-        this.FlushMatProperties(this.sprite);
-        this.PlayMusicAndStartAnalyse();      
+        
+    }
+
+    public WriteFrame(fftTexture: Uint8Array, frame: number, buff: Uint8Array) {
+        // 每个采样32长度
+        // 纹理宽512，所以一行保存16个采样
+        // 外部确保能整除，否则会有很多空间浪费
+        let sampleLength = buff.length;
+        let samplePerRow = 512 / sampleLength;
+        
+        //@ts-ignore
+        let audio: AudioBuffer = this.clip._audio;
+        let elapseInSec = audio.length / audio.sampleRate;
+        let samples = Math.floor(elapseInSec * 60);
+
+        // let sx = frame % 16 * sampleLength;
+        // let sy = frame / 16;
+        let sx = frame * sampleLength;
+        if (sx + sampleLength > fftTexture.length) {
+            console.error("fftTexture overflow");
+            return;
+        }
+
+        fftTexture.set(buff, sx);
+    }
+
+    public Run() {
+        //this.FlushMatProperties(this.sprite);
+        //this.PlayMusicAndStartAnalyse();
+
+        this.Test();
+    }
+
+    protected Test() {
+        //@ts-ignore
+        let audio: AudioBuffer = this.clip._audio;
+        let offlineAudioCtx = new OfflineAudioContext(audio.numberOfChannels, audio.length, audio.sampleRate);
+        let analyser = this._analyser = offlineAudioCtx.createAnalyser();// new AnalyserNode(offlineAudioCtx);
+        analyser.fftSize = this._freqSize * 2;
+
+        let sourceNode = this._sourceNode = offlineAudioCtx.createBufferSource();
+        sourceNode.buffer = audio;
+
+        //let gain = offlineAudioCtx.createGain();
+
+
+        sourceNode.connect(analyser);
+        analyser.connect(offlineAudioCtx.destination);
+            //.connect(gain);
+
+        let elapseInSec = audio.length / audio.sampleRate;
+        let samples: number = Math.floor(elapseInSec * 60);
+        let freqBuff = this._freqBuff = new Uint8Array(analyser.frequencyBinCount);
+        let fftTexture = this._fftTexture = new Uint8Array(samples * analyser.frequencyBinCount);        
+        
+        // 60帧每秒采样
+        for (let i = 0; i < samples; ++i) {
+            offlineAudioCtx.suspend(i / 60).then(() => {
+                // gain.gain.setValueAtTime()
+                // analyser.getByteTimeDomainData(freqBuff);
+                analyser.getByteFrequencyData(freqBuff);
+                that.WriteFrame(fftTexture, i, freqBuff);
+                /*if (i < 20) {
+                    console.log(`${offlineAudioCtx.currentTime}`);
+                    console.log(`data[0]: ${freqBuff[0]}`);
+                }*/
+            }).then(() => {
+                offlineAudioCtx.resume();
+            });
+        }
+
+        /*let processor = offlineAudioCtx.createScriptProcessor(this._freqSize * 2);
+        let buff = offlineAudioCtx.createBufferSource();
+        buff.connect(processor);
+        processor.onaudioprocess = (ev: AudioProcessingEvent) => {
+            let k = 0;
+        };*/
+
+        /*let audioWorklet = offlineAudioCtx.audioWorklet;
+        audioWorklet.addModule*/
+        let that = this;
+        offlineAudioCtx.startRendering();
+        offlineAudioCtx.oncomplete = (ev: OfflineAudioCompletionEvent) => {
+            sourceNode.stop();
+            that.Replay();
+        };
+
+        sourceNode.start(0);
+    }
+
+    protected Replay() {
+
     }
 
     protected PlayMusicAndStartAnalyse() {
@@ -98,13 +189,25 @@ export default class SceneVisualizeMusic extends cc.Component {
         cc.audioEngine.stopMusic();
     }
 
+    protected _kk = 0;
     update() {
+        let t = cc.audioEngine.getCurrentTime(this._audioId);
+        let row = Math.floor(t * 60);
+        let mat = this.sprite.getMaterial(0);
+        if (mat) {
+            mat.setProperty("row", row);
+        }
+        
+        return;
         let analyser = this._analyser;
         if (!analyser || !this._freqBuff)
             return;
 
         // 获取频域数据
         analyser.getByteFrequencyData(this._freqBuff);
+        if (++this._kk < 20)
+            console.log(`data[0]: ${this._freqBuff[0]}`);
+
 
         // 获取时域数据
         //analyser.getByteTimeDomainData(this._freqBuff);
