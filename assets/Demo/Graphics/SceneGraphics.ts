842567779b374576510d75200045746308decfda
Cao Gaoting <caogtaa@gmail.com>
2021-08-12 18:49:58 +0800

    show grid mesh

--- assets/Demo/Graphics/SceneGraphics.ts
+++ assets/Demo/Graphics/SceneGraphics.ts
@@ -60,7 +60,52 @@ export default class SceneGraphics extends cc.Component {
             this.UpdateDisplayMatProperties();
         }
 
+        this.InjectAssembler();
+
         this.NextGraph();
+    }
+
+    protected InjectAssembler() {
+        let ctx = this.ctx;
+
+        let assembler = ctx._assembler;
+        //@ts-ignore
+        let originFn = assembler._vset;
+
+        //@ts-ignore
+        let gfx = cc.gfx;
+        let vfmtPosIndexSdf = new gfx.VertexFormat([
+            { name: gfx.ATTR_POSITION, type: gfx.ATTR_TYPE_FLOAT32, num: 2 },
+            { name: 'a_index', type: gfx.ATTR_TYPE_FLOAT32, num: 1 },
+            { name: 'a_dist', type: gfx.ATTR_TYPE_FLOAT32, num: 1 },
+        ]);
+        
+        vfmtPosIndexSdf.name = 'vfmtPosIndexSdf';
+        assembler.getVfmt = () => {
+            return vfmtPosIndexSdf;
+        };
+
+        //@ts-ignore
+        assembler._vset = (x, y, distance = 0) => {
+            //@ts-ignore
+            let buffer = assembler._buffer;
+            let meshbuffer = buffer.meshbuffer;
+            //@ts-ignore
+            let dataOffset = buffer.vertexStart * assembler.getVfmtFloatCount();
+    
+            let vData = meshbuffer._vData;
+            let uintVData = meshbuffer._uintVData;
+    
+            vData[dataOffset] = x;
+            vData[dataOffset+1] = y;
+            //@ts-ignore
+            // uintVData[dataOffset+2] = assembler._curColor;
+            vData[dataOffset+2] = Math.floor(buffer.vertexStart);
+            vData[dataOffset+3] = distance;
+    
+            buffer.vertexStart ++;
+            meshbuffer._dirty = true;
+        };
     }
 
     update() {
