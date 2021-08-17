e7e2cd759ef49b3e88c0423d09d0df2f833a0434
Cao Gaoting <caogtaa@gmail.com>
2021-08-18 01:16:44 +0800

    solve simple case

--- assets/Demo/Graphics/Script/SmoothTrailAssembler.ts
+++ assets/Demo/Graphics/Script/SmoothTrailAssembler.ts
@@ -53,6 +53,8 @@ export class SmoothTrailAssembler extends cc.Assembler {
     _bufferOffset = 0;
     _curColor = 0;
 
+    _trailBuff = null;
+
     constructor(graphics) {
         // super(graphics);
         super();
@@ -692,14 +694,378 @@ export class SmoothTrailAssembler extends cc.Assembler {
         meshbuffer._dirty = true;
     }
 
-    strokeV2(graphics) {
+    strokeV2(graphics, sp, ep) {
         // this._curColor = graphics._strokeColor._val;
 
-        this._flattenPathsV2(graphics._impl);
-        this._expandStrokeV2(graphics);
+        this._flattenPathsV2(graphics._impl, sp, ep);
+        this._expandStrokeV2(graphics, sp, ep);
 
         // 永远不更新PathOffset
         // graphics._impl._updatePathOffset = true;
+    }
+
+    // 通过判断收尾点是否相同，判定闭环，如果是，则弹出最后一个点
+    // 求每个点到下个点的距离、单位方向向量
+    _flattenPathsV2(impl, sp, ep) {
+        let paths = impl._paths;
+        let i = impl._pathOffset;
+        let path = paths[i];
+        let pts = path.points;
+
+        // let p0 = pts[pts.length - 1];
+        // let p1 = pts[0];
+
+        // if (pts.length > 2 && p0.equals(p1)) {
+        //     path.closed = true;
+        //     pts.pop();
+        //     p0 = pts[pts.length - 1];
+        // }
+
+        // for (let j = 0, size = pts.length; j < size; j++) {
+        // ep点的dx是要计算的
+        for (let j = sp; j <= ep; ++j) {
+            let p0 = pts[j];
+            let p1 = pts[j+1];
+            // Calculate segment direction and length
+            let dPos = p1.sub(p0);
+            p0.len = dPos.mag();
+            if (dPos.x || dPos.y)
+                dPos.normalizeSelf();
+            p0.dx = dPos.x;
+            p0.dy = dPos.y;
+            // Advance
+            // p0 = p1;
+            // p1 = pts[j + 1];
+        }
+    }
+
+    // stroke pntIndex -> pntIndex+1
+    _expandStrokeV2(graphics, sp, ep) {
+        let w = graphics.lineWidth * 0.5,
+            lineCap = graphics.lineCap,
+            lineJoin = graphics.lineJoin,
+            miterLimit = graphics.miterLimit;
+
+        let impl = graphics._impl;
+
+        let ncap = curveDivs(w, PI, impl._tessTol);
+
+        this._calculateJoinsV2(impl, w, lineJoin, miterLimit, sp, ep);
+
+        let paths = impl._paths;
+
+        // // Calculate max vertex usage.
+        // let cverts = 0;
+        // for (let i = impl._pathOffset, l = impl._pathLength; i < l; i++) {
+        //     let path = paths[i];
+        //     let pointsLength = path.points.length;
+
+        //     if (lineJoin === LineJoin.ROUND) cverts += (pointsLength + path.nbevel * (ncap + 2) + 1) * 2; // plus one for loop
+        //     else cverts += (pointsLength + path.nbevel * 5 + 1) * 2; // plus one for loop
+
+        //     if (!path.closed) {
+        //         // space for caps
+        //         if (lineCap === LineCap.ROUND) {
+        //             cverts += (ncap * 2 + 2) * 2;
+        //         } else {
+        //             cverts += (3 + 3) * 2;
+        //         }
+        //     }
+        // }
+
+        // TODO: 由于此时的cverts是动态增长的，buff预先分配2048，后续
+        // 1. 使用多个buff拼接实现更长的mesh
+        // 2. 用循环数组形式移除最老的verts
+        // 在cap start里应该完成这个事情
+        // let cverts = 2048;
+
+        // let buffer = this._trailBuff;
+        // if (!buffer) {
+        //     buffer = this._trailBuff = this.genBuffer(graphics, cverts);
+        // }
+        // let // buffer = this.genBuffer(graphics, cverts),
+        //     meshbuffer = buffer.meshbuffer,
+        //     vData = meshbuffer._vData,
+        //     iData = meshbuffer._iData;
+
+        let i = impl._pathOffset;
+        let path = paths[i];
+        let pts = path.points;
+        // let pointsLength = pts.length;
+        // let offset = buffer.vertexStart;
+
+        // let p0, p1;
+        // let start, end, loop;
+        // loop = path.closed;
+        // if (loop) {
+        //     // Looping
+        //     p0 = pts[pointsLength - 1];
+        //     p1 = pts[0];
+        //     start = 0;
+        //     end = pointsLength;
+        // } else {
+        //     // Add cap
+        //     p0 = pts[0];
+        //     p1 = pts[1];
+        //     start = 1;
+        //     end = pointsLength - 1;
+        // }
+
+        // p1 = p1 || p0;
+
+        // if (!loop) {
+        //     // Add cap
+        //     let dPos = p1.sub(p0);
+        //     dPos.normalizeSelf();
+
+        //     let dx = dPos.x;
+        //     let dy = dPos.y;
+
+        //     if (lineCap === LineCap.BUTT)
+        //         this._buttCapStart(p0, dx, dy, w, 0);
+        //     else if (lineCap === LineCap.SQUARE)
+        //         this._buttCapStart(p0, dx, dy, w, w);
+        //     else if (lineCap === LineCap.ROUND)
+        //         this._roundCapStart(p0, dx, dy, w, ncap);
+        // }
+
+        for (let j = sp; j < ep; ++j) {
+            let p0 = pts[j];
+            let p1 = pts[j+1];
+            if (lineJoin === LineJoin.ROUND) {
+                this._roundJoin(p0, p1, w, w, ncap);
+            }
+            else if ((p1.flags & (PointFlags.PT_BEVEL | PointFlags.PT_INNERBEVEL)) !== 0) {
+                this._bevelJoin(p0, p1, w, w);
+            }
+            else {
+                this._vset(p1.x + p1.dmx * w, p1.y + p1.dmy * w, 1);
+                this._vset(p1.x - p1.dmx * w, p1.y - p1.dmy * w, -1);
+            }
+
+            //p0 = p1;
+            //p1 = pts[j + 1];
+        }
+
+        // if (loop) {
+        //     // Loop it
+        //     let floatCount = this.getVfmtFloatCount();
+        //     let vDataoOfset = offset * floatCount;
+        //     this._vset(vData[vDataoOfset], vData[vDataoOfset + 1], 1);
+        //     this._vset(vData[vDataoOfset + floatCount], vData[vDataoOfset + floatCount + 1], -1);
+        // } else {
+        //     // Add cap
+        //     let dPos = p1.sub(p0);
+        //     dPos.normalizeSelf();
+
+        //     let dx = dPos.x;
+        //     let dy = dPos.y;
+
+        //     if (lineCap === LineCap.BUTT)
+        //         this._buttCapEnd(p1, dx, dy, w, 0);
+        //     else if (lineCap === LineCap.SQUARE)
+        //         this._buttCapEnd(p1, dx, dy, w, w);
+        //     else if (lineCap === LineCap.ROUND)
+        //         this._roundCapEnd(p1, dx, dy, w, ncap);
+        // }
+
+        this.FlushIndices(graphics);
+    }
+
+    protected FlushIndices(graphics) {
+        let cverts = 2048;
+
+        let buffer = this._trailBuff;
+        if (!buffer) {
+            buffer = this._trailBuff = this.genBuffer(graphics, cverts);
+        }
+        let // buffer = this.genBuffer(graphics, cverts),
+            meshbuffer = buffer.meshbuffer,
+            vData = meshbuffer._vData,
+            iData = meshbuffer._iData;
+
+        // todo: 连接前面的Mesh
+        // offset是更新前
+        // stroke indices
+        let indicesOffset = buffer.indiceStart;
+        let offset = this._vertexHead;
+        if (offset == this._pathVertexStart) {
+            // 如果是开头，不需要衔接其他内容
+            offset += 2;
+        }
+        for (let start = offset, end = buffer.vertexStart; start < end; start++) {
+            iData[indicesOffset++] = start - 2;
+            iData[indicesOffset++] = start - 1;
+            iData[indicesOffset++] = start;
+        }
+
+        this._vertexHead = buffer.vertexStart;
+        buffer.indiceStart = indicesOffset;
+    }
+
+    // todo: calculate range
+    _calculateJoinsV2(impl, w, lineJoin, miterLimit, sp, ep) {
+        let iw = 0.0;
+        let w2 = w * w;
+
+        if (w > 0.0) {
+            iw = 1 / w;
+        }
+
+        // Calculate which joins needs extra vertices to append, and gather vertex count.
+        let paths = impl._paths;
+        let i = impl._pathOffset;
+        let path = paths[i];
+
+        let pts = path.points;
+        let ptsLength = pts.length;
+        // let p0 = pts[ptsLength - 1];
+        // let p1 = pts[0];
+        let nleft = 0;
+
+        // path.nbevel = 0;     // do not init
+
+        // for (let j = 0; j < ptsLength; j++) {
+        for (; sp < ep; ++sp) {
+            let p0 = pts[sp];
+            let p1 = pts[sp+1];
+            let dmr2, cross, limit;
+
+            // perp normals
+            let dlx0 = p0.dy;
+            let dly0 = -p0.dx;
+            let dlx1 = p1.dy;
+            let dly1 = -p1.dx;
+
+            // Calculate extrusions
+            p1.dmx = (dlx0 + dlx1) * 0.5;
+            p1.dmy = (dly0 + dly1) * 0.5;
+            dmr2 = p1.dmx * p1.dmx + p1.dmy * p1.dmy;
+            if (dmr2 > 0.000001) {
+                let scale = 1 / dmr2;
+                if (scale > 600) {
+                    scale = 600;
+                }
+                p1.dmx *= scale;
+                p1.dmy *= scale;
+            }
+
+            // Keep track of left turns.
+            cross = p1.dx * p0.dy - p0.dx * p1.dy;
+            if (cross > 0) {
+                nleft++;
+                p1.flags |= PointFlags.PT_LEFT;
+            }
+
+            // Calculate if we should use bevel or miter for inner join.
+            limit = max(11, min(p0.len, p1.len) * iw);
+            if (dmr2 * limit * limit < 1) {
+                p1.flags |= PointFlags.PT_INNERBEVEL;
+            }
+
+            // TODO: 2.4.4的commit，会导致INNERBEVEL渲染错误
+            // https://github.com/cocos-creator/engine/pull/7780/commits/06320339dae5419a6e96058344a35254429862f1
+            // Check whether dm length is too long
+            let dmwx = p1.dmx * w;
+            let dmwy = p1.dmy * w;
+            let dmlen2 = dmwx * dmwx + dmwy * dmwy;
+            // 设交点P到dm点的连线为S，和len、w围成三角形。S对应的角是O
+            // 只要O不是钝角，则dm点一定在线段内部。极限情况下O是直角，此时根据勾股定理求S的最大容忍值
+            if (dmlen2 > (p1.len * p1.len) + w2 && dmlen2 > (p0.len * p0.len) + w2) {
+                p1.flags |= PointFlags.PT_INNERBEVEL;
+            }
+
+            // Check to see if the corner needs to be beveled.
+            if (p1.flags & PointFlags.PT_CORNER) {
+                if (dmr2 * miterLimit * miterLimit < 1 || lineJoin === LineJoin.BEVEL || lineJoin === LineJoin.ROUND) {
+                    p1.flags |= PointFlags.PT_BEVEL;
+                }
+            }
+
+            if ((p1.flags & (PointFlags.PT_BEVEL | PointFlags.PT_INNERBEVEL)) !== 0) {
+                path.nbevel++;
+            }
+        }
+    }
+
+    protected _pathVertexStart: number = 0;
+    protected _vertexHead: number = 0;
+    public CapStart(graphics, sp: number) {
+        let cverts = 2048;
+        let buffer = this._trailBuff;
+        if (!buffer)
+            buffer = this._trailBuff = this.genBuffer(graphics, cverts);
+
+        // 记录整条path的开始vertex
+        // 主要用于在strok时顺便初始化startCap的索引
+        this._vertexHead = this._pathVertexStart = buffer.vertexStart;
+
+        let impl = graphics._impl;
+        let i = impl._pathOffset;
+        let paths = impl._paths;
+        let path = paths[i];
+
+        let pts = path.points;
+
+        let w = graphics.lineWidth * 0.5,
+            lineCap = graphics.lineCap,
+            lineJoin = graphics.lineJoin,
+            miterLimit = graphics.miterLimit;
+        let ncap = curveDivs(w, PI, impl._tessTol);
+
+        // todo: dx, dy已经有了，不要再算，call flatten
+        let p0 = pts[sp];
+        let p1 = pts[sp+1];
+
+        // Add cap
+        let dPos = p1.sub(p0);
+        dPos.normalizeSelf();
+
+        let dx = dPos.x;
+        let dy = dPos.y;
+
+        if (lineCap === LineCap.BUTT)
+            this._buttCapStart(p0, dx, dy, w, 0);
+        else if (lineCap === LineCap.SQUARE)
+            this._buttCapStart(p0, dx, dy, w, w);
+        else if (lineCap === LineCap.ROUND)
+            this._roundCapStart(p0, dx, dy, w, ncap);
+
+        // 没必要，但是是幂等的
+        // this.FlushIndices(graphics);
+    }
+
+    public CapEnd(graphics, sp: number) {
+        let impl = graphics._impl;
+        let i = impl._pathOffset;
+        let paths = impl._paths;
+        let path = paths[i];
+
+        let pts = path.points;
+
+        let w = graphics.lineWidth * 0.5,
+            lineCap = graphics.lineCap,
+            lineJoin = graphics.lineJoin,
+            miterLimit = graphics.miterLimit;
+        let ncap = curveDivs(w, PI, impl._tessTol);
+
+        // todo: dx, dy已经有了，不要再算
+        let p0 = pts[sp];
+        let p1 = pts[sp+1];
+        let dPos = p1.sub(p0);
+        dPos.normalizeSelf();
+
+        let dx = dPos.x;
+        let dy = dPos.y;
+
+        if (lineCap === LineCap.BUTT)
+            this._buttCapEnd(p1, dx, dy, w, 0);
+        else if (lineCap === LineCap.SQUARE)
+            this._buttCapEnd(p1, dx, dy, w, w);
+        else if (lineCap === LineCap.ROUND)
+            this._roundCapEnd(p1, dx, dy, w, ncap);
+
+        this.FlushIndices(graphics);
     }
 }
 
